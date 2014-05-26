056353c63d7904abb78e84b1be7884b130adb1e1
Darshaka Pathirana <dpat@syn-net.org>
2014-05-26 11:43:03 +0200

    print URL of last minutely diff

--- scripts/csn.py
+++ scripts/csn.py
@@ -3,8 +3,14 @@
 # download stats.txt and output current sequence number
 
 import urllib2, re
+
+base_minurl = 'http://planet.openstreetmap.org/replication/minute/000/'
+
 response = urllib2.urlopen('http://planet.openstreetmap.org/replication/minute/state.txt')
 html = response.read()
 sequenceNumber = re.findall('.*sequenceNumber=\d*', html)[0]
 currentSequnceNumber = re.split('=', sequenceNumber)[1]
-print currentSequnceNumber
+m = re.search('(...)(...)', currentSequnceNumber)
+
+if m:
+    print base_minurl + m.group(1) + "/" + m.group(2) + ".osc.gz"
