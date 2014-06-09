e0f48c1401ae74ed8607417f7c8eaa3b059d59f6
Darshaka Pathirana <dpat@syn-net.org>
2014-06-09 12:07:45 +0200

    implemented getCurrentSequenceNumber()

--- scripts/regional-diff.py
+++ scripts/regional-diff.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import argparse
+import argparse, urllib2, re
 
 parser = argparse.ArgumentParser(\
                 formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -8,6 +8,12 @@ parser = argparse.ArgumentParser(\
 planet.openstreetmap.org - replication diff file',
                 epilog='''
 ''')
+
+# TODO: figure out what happens when 000 changes. Currently (2014-06-09)
+# state.txt does not show 000 in it.
+replication_url = 'http://planet.openstreetmap.org/replication/'
+minutely_url = replication_url + "minute/"
+state_url = minutely_url + "state.txt"
 
 parser.add_argument("--verbose", action="store_true", help="increase verbosity")
 args = parser.parse_args()
@@ -23,6 +29,14 @@ if args.verbose:
 else:   
     verboseprint = lambda *a: None      # do-nothing function
 
+# download stats.txt and extract current sequence number
+def getCurrentSequenceNumber():
+    response = urllib2.urlopen(state_url)
+    html = response.read()
+    verboseprint("VERBOSE: Content of state.txt:\n", html)
+    sequenceNumber = re.findall('.*sequenceNumber=\d*', html)[0]
+    return re.split('=', sequenceNumber)[1]
+
 if __name__ == '__main__':
-    print "Currently no action"
+    print "CurrentSequenceNumber: " + getCurrentSequenceNumber()
 
